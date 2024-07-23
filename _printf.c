472c009d3682a388dd0cc2169f2599835a3e9072
root <dylan2003gd@gmail.com>
2024-07-23 12:55:24 +0000

    corrct error

--- _printf.c
+++ _printf.c
@@ -5,29 +5,32 @@
 
 int _printf(const char *format, ...)
 {
-        int num_char = 0;
-        va_list args;
-        va_start(args, format);
-        char opt[2];
+	int num_char = 0;
+	va_list args;
+	va_start(args, format);
+	char opt[2];
+	int (*formatter)(va_list);
 
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        opt[2] = {*format, '\n'}
-                        formatter = get_format_func(opt);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			opt[0] = *format;
+			opt[1] = '\0';
+			formatter = get_format_func(opt);
 
-                        if (formatter)
-                                num_char += formatter(args);
-                }
-                else
-                {
-                        _putchar(*format);
-                        num_char++;
-                }
-                format++;
-        }
-        va_end(args);
-        return (num_char);
+			if (formatter)
+				num_char += formatter(args);
+		}
+		
+		else
+		{
+			_putchar(*format);
+			num_char++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (num_char);
 }
