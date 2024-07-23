5d2c7fb0dca2dd37870b2226d7845a937e58d8f6
S1even <Stevendevin95520@hotmail.com>
2024-07-23 15:21:14 +0200

    Try to correct a error

--- _printf.c
+++ _printf.c
@@ -35,3 +35,20 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (num_char);
 }
+
+
+int (*get_format_funct(char opt))(va_list)
+{
+	int i;
+
+	format_types fopt[] = {
+		{"c", _printf_char},
+		{"s", _printf_string},
+		{"%", _printf_percent},
+	};
+
+	for (i = 0; i < 3; i++)
+		if (fopt[i].op[0] == opt)
+			return (fopt[i].f);
+	return (NULL);
+}
