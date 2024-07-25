b5b03ed2810ce0af021639513a83735277efacf1
root <dylan2003gd@gmail.com>
2024-07-25 08:29:43 +0000

    delete 2 functions

--- main.h
+++ main.h
@@ -11,11 +11,7 @@ int _printf_char(va_list args);
 int _printf_string(va_list args);
 int _printf_percent(va_list args);
 int _printf_integer(va_list args);
-int __printf_integer(int nb, int count);
 int _putchar(char c);
-
-int _abs(int c);
-
 int handle_format(char format, va_list args);
 
 #endif
